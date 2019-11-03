dd9d899d79316f697fe353364efa98dec9d9a8f3
Jan Erik Hanssen <jhanssen@gmail.com>
2019-11-02 19:26:28 -0700

    typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,47 @@
-console.log('Hello world!');
+import * as xdgBaseDir from "xdg-basedir";
+import { default as Options } from "@jhanssen/options";
+import { default as native, Data as OwmData, Event as OwmEvent, XCB } from "../native";
+import * as path from "path";
+
+const options = Options("owm");
+
+function loadConfig(dir: string)
+{
+    import(path.join(dir, "owm")).then(cfg => {
+        cfg({});
+    }).catch(() => {});
+}
+
+xdgBaseDir.configDirs.forEach(dir => { loadConfig(dir) });
+
+console.log("faff", native);
+
+let owm: OwmData;
+
+function event(e: OwmEvent) {
+    console.log("got event", e);
+
+    if (e.type == "xcb" && e.xcb) {
+        const xcb = e.xcb;
+        if (xcb.type === owm.xcb.event.BUTTON_PRESS) {
+            const press = xcb as XCB.ButtonPress;
+            const config = {
+                window: 0x20000d,
+                x: press.root_x,
+                y: press.root_y
+            };
+            try {
+                owm.xcb.configure_window(owm.wm, config);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+}
+
+native.start(event).then((data: OwmData) => {
+    console.log("started", data);
+    owm = data;
+}).catch((err: Error) => {
+    console.log("error", err);
+});
