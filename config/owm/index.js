cca368af885a93e2b07e4ff5cd2408ac0bd13490
Jan Erik Hanssen <jhanssen@gmail.com>
2019-11-18 20:11:35 -0800

    update config file to new apis

--- config/owm/index.js
+++ config/owm/index.js
@@ -2,104 +2,126 @@ let logger;
 
 function init(owmlib) {
     logger = owmlib.logger.prefixed("config");
-    logger.info("hello... adding ctrl-a");
 
-    owmlib.bindings.add("Ctrl+A", (bindings, binding) => {
-        //logger.info("got", binding);
-        // activate workspace 1
-        const ws = owmlib.monitors.workspaceById(1);
-        if (!ws) {
-            throw new Error("no workspace 1");
+    owmlib.events.on("inited", () => {
+        logger.error("owm inited");
+
+        const yabar = owmlib.findClientByName("yabar");
+        if (!yabar) {
+            // launch it
+            owmlib.launch("yabar");
         }
-        ws.monitor.workspace = ws;
     });
 
-    owmlib.bindings.add("Ctrl+B", (bindings, binding) => {
-        //logger.info("got", binding);
-        // activate workspace 2
-        const ws = owmlib.monitors.workspaceById(2);
-        if (!ws) {
-            throw new Error("no workspace 2");
-        }
-        ws.monitor.workspace = ws;
-    });
+    const mod = "Ctrl";
 
-    owmlib.bindings.add("Ctrl+M", (bindings, binding) => {
+    // general bindings
+    owmlib.bindings.add(`${mod}+Shift+Q`, () => {
         const client = owmlib.findClientUnderCursor();
         if (client) {
-            logger.error("moving client by keyboard");
-            client.moveByKeyboard();
+            client.kill();
         }
     });
 
-    owmlib.bindings.add("Ctrl+R", (bindings, binding) => {
-        const client = owmlib.findClientUnderCursor();
-        if (client) {
-            logger.error("resizing client by keyboard");
-            client.resizeByKeyboard();
-        }
-    });
+    // workspaces
+    for (let i = 1; i < 10; ++i) {
+        // switch workspace
+        owmlib.bindings.add(`${mod}+Ctrl+${i}`, () => {
+            const ws = owmlib.monitors.workspaceById(i);
+            if (ws) ws.activate();
+        });
+        // move to workspace
+        owmlib.bindings.add(`${mod}+Shift+${i}`, () => {
+            const ws = owmlib.monitors.workspaceById(i);
+            const client = owmlib.findClientUnderCursor();
+            if (ws && client) client.workspace = ws;
+        });
+    }
 
-    const ctrldmode = new owmlib.KeybindingsMode(owmlib, "Ctrl+D mode");
-    ctrldmode.add("Escape", (mode) => {
-        logger.info("exit ctrldmode");
-        mode.exit();
-    });
-    ctrldmode.add("Ctrl+A", () => {
+    // launch applications
+    owmlib.bindings.add(`${mod}+T`, () => {
         owmlib.launch("terminator");
     });
-    ctrldmode.add("G", () => {
-        logger.error("pressed G");
-    });
-    owmlib.bindings.addMode("Ctrl+D", ctrldmode);
 
-    owmlib.bindings.add("Ctrl+T", () => {
-        owmlib.launch("terminator");
+    owmlib.bindings.add(`${mod}+G`, () => {
+        owmlib.launch("google-chrome");
     });
 
     owmlib.policy.layout = owmlib.policy.createLayout("tiling");
     const cfg = new owmlib.policy.layout.Config();
 
-    cfg.rows = 2;
+    cfg.rows = 1;
     cfg.columns = undefined;
 
-    owmlib.policy.layout.setConfig(cfg);
+    owmlib.policy.layout.config = cfg;
+
+    const ratios = new WeakMap();
+    const resizeMode = new owmlib.KeybindingsMode(owmlib, "Resize mode");
+    resizeMode.add("Escape", (mode) => {
+        logger.info("exit resizeMode");
+        mode.exit();
+    });
+    resizeMode.add("Return", (mode) => {
+        logger.info("exit resizeMode");
+        mode.exit();
+    });
+    resizeMode.add("Left", () => {
+        // find the current container
+        const container = owmlib.findContainerUnderCursor();
+        if (container) {
+            let ratio = ratios.get(container);
+            if (ratio === undefined) {
+                ratio = { r: 1 };
+                ratios.set(container, ratio);
+            }
+
+            if (ratio.r >= 0.5) {
+                ratio.r -= 0.1;
+                container.layoutPolicy.config.setColumRatio(0, ratio.r);
+            }
+        }
+    });
+    resizeMode.add("Right", () => {
+        const container = owmlib.findContainerUnderCursor();
+        if (container) {
+            let ratio = ratios.get(container);
+            if (ratio === undefined) {
+                ratio = { r: 1 };
+                ratios.set(container, ratio);
+            }
+
+            if (ratio.r <= 1.5) {
+                ratio.r += 0.1;
+                container.layoutPolicy.config.setColumRatio(0, ratio.r);
+            }
+        }
+    });
+    owmlib.bindings.addMode(`${mod}+Z`, resizeMode);
 
     owmlib.activeColor = "#33c";
     owmlib.moveModifier = "Ctrl";
-
-    const xtermMatchCondition = new owmlib.Match.MatchWMClass({ class: "XTerm" });
-    const xtermMatch = new owmlib.Match((client) => {
-        console.log("got an xterm, adding to ws 2");
-        const ws = owmlib.monitors.workspaceById(2);
-        if (!ws) {
-            throw new Error("no workspace 2");
-        }
-        client.workspace = ws;
-    });
-    xtermMatch.addCondition(xtermMatchCondition);
-    owmlib.addMatch(xtermMatch);
 
     owmlib.events.on("monitors", monitors => {
         // console.log("got screens?", screens, owmlib.Workspace);
         if (monitors.added) {
             for (const [key, monitor] of monitors.added) {
-                const ws1 = new owmlib.Workspace(owmlib, 1);
-                monitor.workspaces.add(ws1);
-                monitor.workspace = ws1;
-
-                const ws2 = new owmlib.Workspace(owmlib, 2);
-                monitor.workspaces.add(ws2);
+                for (let i = 1; i <= 5; ++i) {
+                    const ws = new owmlib.Workspace(owmlib, i);
+                    console.log("adding ws for", i);
+                    monitor.workspaces.add(ws);
+                    if (i === 1) {
+                        monitor.workspace = ws;
+                    }
+                }
             }
         }
     });
     owmlib.events.on("client", client => {
-        logger.info("got client");
         if (client.workspace || client.ignoreWorkspace)
             return;
         const m = owmlib.monitors.monitorByOutput("default");
         if (!m) {
-            throw new Error("no default workspace");
+            throw new Error("no default monitor");
         }
         client.workspace = m.workspace;
     });
