40d70bc544b7c8527331f3f28cf63c5607a13859
Anders Bakken <agbakken@gmail.com>
2019-11-25 12:39:36 -0800

    Support date format in clock.

--- applets/bar/modules/clock.ts
+++ applets/bar/modules/clock.ts
@@ -2,25 +2,24 @@ import { Graphics } from "../../../native";
 import { OWMLib, Geometry } from "../../../lib";
 import { Bar, BarModule, BarModuleConfig } from "..";
 import { EventEmitter } from "events";
+import { default as dateFormat } from "dateformat";
 
 export class ClockConfig implements BarModuleConfig
 {
     constructor() { }
 
     textColor?: string;
-    seconds?: boolean;
-    prefix?: string;
     font?: string;
+    format?: string;
+    timeout?: number;
 }
 
 export class Clock extends EventEmitter implements BarModule
 {
     private _config: ClockConfig;
-    private _dateOptions: { hour: string, minute: string, second?: string, hour12: boolean };
     private _clock: Graphics.Text;
     private _color: { red: number, green: number, blue: number, alpha: number };
     private _metrics: { width: number, height: number };
-    private _prefix: string;
 
     constructor(owm: OWMLib, bar: Bar, config: BarModuleConfig) {
         super();
@@ -30,32 +29,23 @@ export class Clock extends EventEmitter implements BarModule
         this._config = clockConfig;
         this._color = Bar.makeColor(clockConfig.textColor || "#fff");
 
-        this._dateOptions = {
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: false
-        };
-        if (clockConfig.seconds) {
-            this._dateOptions.second = "2-digit";
-        }
-
-        this._prefix = clockConfig.prefix || "";
+        const str = dateFormat(new Date(), this._config.format || "HH:MM");
 
         this._clock = owm.engine.createText(bar.ctx);
         owm.engine.textSetFont(this._clock, clockConfig.font || "Sans Bold 10");
-        owm.engine.textSetText(this._clock, this._prefix + (clockConfig.seconds ? "00:00:00" : "00:00"));
+        owm.engine.textSetText(this._clock, str);
         this._metrics = owm.engine.textMetrics(this._clock);
 
         setInterval(() => {
             this.emit("updated");
-        }, clockConfig.seconds ? 1000 : 1000 * 60);
+        }, this._config.timeout || 1000 * 60);
     }
 
     paint(engine: Graphics.Engine, ctx: Graphics.Context, geometry: Geometry) {
         const { red, green, blue } = this._color;
         engine.setSourceRGB(ctx, red, green, blue);
-        const txt = this._prefix + (new Date()).toLocaleTimeString("en-US", this._dateOptions);
-        engine.textSetText(this._clock, txt);
+
+        engine.textSetText(this._clock, dateFormat(new Date(), this._config.format || "HH:MM"));
         engine.drawText(ctx, this._clock);
     }
 
