83cdd7faa24e7980e042cfa0fff99d340f1ac7e2
Anders Bakken <agbakken@gmail.com>
2019-11-25 12:39:03 -0800

    Don't default log level.

--- scripts/runowm.sh
+++ scripts/runowm.sh
@@ -4,7 +4,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 while true; do
     cd $SCRIPTPATH/..
-    npm run start -- --log=debug "$@"
+    npm run start -- "$@"
     if [ $? -eq 0 ]; then
         break
     fi
